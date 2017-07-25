3747021ba9c794f7f435f2cdaec6d819e02e0edf
brianbianco <brian.bianco@gmail.com>
2017-07-25 11:57:49 -0400

    Quickly hack in writing directly to kinesis stream

--- main.go
+++ main.go
@@ -10,6 +10,9 @@ import (
 	"sync"
 	"syscall"
 	"time"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/kinesis"
 )
 
 const TimeFormat = "20060102150405.000"
@@ -32,11 +35,52 @@ func main() {
 			c <- "I should probabyl die now!!!"
 		}
 	}(chans)
-	wg.Add(3)
-	go write_vomit("./stream1.dat", &wg, &chans[0])
-	go write_vomit("./stream2.dat", &wg, &chans[1])
-	go write_vomit("./stream3.dat", &wg, &chans[2])
+	s := session.New(&aws.Config{Region: aws.String("us-west-2")})
+	kc := kinesis.New(s)
+  streamName := "cm_message_kinesis_kinesis_stream"
+
+	  wg.Add(3)
+    go write_kinesis(*kc, &streamName, &wg, &chans[0])
+    go write_kinesis(*kc, &streamName, &wg, &chans[1])
+    go write_kinesis(*kc, &streamName, &wg, &chans[2])
+
+//	go write_vomit("./stream1.dat", &wg, &chans[0])
+//	go write_vomit("./stream2.dat", &wg, &chans[1])
+//	go write_vomit("./stream3.dat", &wg, &chans[2])
 	wg.Wait()
+}
+
+func write_kinesis(kc kinesis.Kinesis, streamName *string, wg *sync.WaitGroup, done *chan string) {
+	titles := NewTitleCollection(1000)
+
+  i := 0
+	finished := false
+	for {
+		select {
+		case <-*done:
+			finished = true
+		default:
+			if finished {
+				fmt.Println("done writing")
+				wg.Done()
+				return
+			}
+      time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+      //write record
+      title  := titles.Pop()
+		  fmt.Println(title)
+			_, err := kc.PutRecord(&kinesis.PutRecordInput{
+        Data:         []byte(title),
+        StreamName:   streamName,
+        PartitionKey: aws.String("None"),
+      })
+      if err != nil {
+        fmt.Println("Could not write to kinesis stream")
+      }
+      i++
+		}
+	}
+	wg.Done()
 }
 
 func write_vomit(fname string, wg *sync.WaitGroup, done *chan string) {
